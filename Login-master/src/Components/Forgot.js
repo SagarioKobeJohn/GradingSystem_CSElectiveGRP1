7e2d5d6ec2ba6292c43660b3f2465d3729b99c8d
SagarioKobeJohn <96183184+SagarioKobeJohn@users.noreply.github.com>
2022-06-10 22:24:48 +0800

    Add files via upload

---
+++ Login-master/src/Components/Forgot.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import { Form, Alert } from "react-bootstrap";
+import Login from "./Login";
+
+function Forgot() {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [flag, setFlag] = useState(false);
+  const [login, setLogin] = useState(true);
+  
+
+
+  function handleFormSubmit(e) {
+    e.preventDefault();
+
+    if (!email || !password ) {
+      setFlag(true);
+    } else {
+      setFlag(false);
+      localStorage.setItem("sanskarEmail", JSON.stringify(email));
+      localStorage.setItem(
+        "sanskarPassword",
+        JSON.stringify(password)
+      );
+      console.log("Saved in Local Storage");
+
+      setLogin(!login);
+    }
+  }
+
+  function handleClick() {
+    setLogin(!login);
+  }
+
+ 
+  
+
+  return (
+    <>
+ 
+        <div>
+          {" "}
+          {login ? (
+            <form onSubmit={handleFormSubmit}>
+              <h3>EDIT</h3>
+
+
+              <div className="form-group">
+                <label>Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control"
+                  placeholder="Enter password"
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </div>             
+
+              <button type="submit" className="btn btn-dark btn-lg btn-block">
+                Register
+              </button>
+              <p onClick={handleClick} className="forgot-password text-right">
+                Already registered{" "}log in?
+                
+              </p>
+              {flag && (
+                <Alert color="primary" variant="danger">
+                  I got it you are in hurry! But every Field is important!
+                </Alert>
+              )}
+            </form>
+          ) : (
+            <Login />
+          )}
+        </div>
+    
+    </>
+  );
+}
+
+export default Forgot;
